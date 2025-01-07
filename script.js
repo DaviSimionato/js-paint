91affdc18c48d7777ef6b0317e9e140e81578f3d
Davi Simionato <davisimionato@hotmail.com>
2025-01-07 11:27:37 -0300

    feat: progress

--- script.js
+++ script.js
@@ -6,21 +6,22 @@ let counter = 0;
 let click = false;
 let grid = true;
 let cursorMode = "paint";
-let color = "#E5E7EB";
+let color = "#6b7280";
+let pColor = "#6b7280";
 let block = null;
 function createCanva() {
     if(grid) {
         for(let i=0;i<820;i++) {
             let obj = document.createElement("div");
-            obj.classList.add(`el-${i}`);
-            obj.classList.add("g-block", "w-[25px]", "h-[25px]", "border", "hover:bg-gray-100");
+            // obj.classList.add(`el-${i}`);
+            obj.classList.add("g-block", "w-[25px]", "h-[25px]", "border", `hover:bg-[${color}]`);
             canva.appendChild(obj);
         }
     }else {
         for(let i=0;i<820;i++) {
             let obj = document.createElement("div");
-            obj.classList.add(`el-${i}`);
-            obj.classList.add("g-block", "w-[25px]", "h-[25px]", "hover:bg-gray-100");
+            // obj.classList.add(`el-${i}`);
+            obj.classList.add("g-block", "w-[25px]", "h-[25px]", `hover:bg-[${color}]`);
             canva.appendChild(obj);
         }
     }
@@ -48,7 +49,14 @@ function blockFuntions() {
         b.addEventListener("mouseenter", ()=> {
             if(click) {
                 if(cursorMode == "paint") {
-                    b.classList.add(`bg-[${color}]`);
+                    while (b.classList.length > 0) {
+                        b.classList.remove(b.classList.item(0));
+                    }
+                    if(grid) {
+                        b.classList.add("g-block", "w-[25px]", "h-[25px]", "border", `hover:bg-[${color}]`, `bg-[${color}]`);
+                    }else {
+                        b.classList.add("g-block", "w-[25px]", "h-[25px]", `hover:bg-[${color}]`, `bg-[${color}]`);
+                    }
                 }
                 if(cursorMode == "eraser") {
                     b.classList.remove(`bg-[${color}]`);
@@ -79,7 +87,14 @@ disableGridBtn.addEventListener("click", ()=> {
 });
 colorPicker.forEach((c)=> {
     c.addEventListener("click", ()=> {
-        // color = c.innerHTML;
-        console.log(c.innerHTML)
+        colorPicker.forEach((c)=> {
+            c.classList.remove("border-black", "border-2");
+        });
+        c.classList.add("border-black", "border-2");
+        pColor = color;
+        color = c.innerHTML;
+        block.forEach((b)=> {
+            b.classList.replace(`hover:bg-[${pColor}]`, `hover:bg-[${color}]`);
+        });
     })
 })
